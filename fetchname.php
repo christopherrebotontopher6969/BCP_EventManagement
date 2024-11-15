c07015144bc31dc3c1b6b1ad7236b3050b79352e
christopherrebotontopher6969 <127385115+christopherrebotontopher6969@users.noreply.github.com>
2024-11-15 11:53:07 +0800

    Update fetchname.php

--- fetchname.php
+++ fetchname.php
@@ -2,10 +2,10 @@
 <?php
 
 
-$servername = "localhost"; 
-$username = "root"; 
-$password = ""; 
-$dbname = "bcp_sms3_ems"; 
+$host = 'localhost';
+$db = 'event_bcp_sms3_ems'; 
+$user = 'event_topher';  
+$pass = 'topher';  
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
